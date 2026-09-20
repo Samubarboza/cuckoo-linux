d492d60ba558a765c0d4c328895c1d667c808e3f
Samubarboza <samu_junior95@hotmail.com>
2026-09-20 00:49:06 -0300

    se agrega cuckoo-configurator con las preguntas de instalacion

--- configs/cuckoo/profiledef.sh
+++ configs/cuckoo/profiledef.sh
@@ -19,6 +19,7 @@ file_permissions=(
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/cuckoo-configurator"]="0:0:755"
   ["/usr/local/bin/cuckoo-hardware"]="0:0:755"
   ["/usr/local/share/cuckoo/hardware/"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
